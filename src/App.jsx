aac9b982d7a5cd1ba0303cbea155fa41c735199f
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-03 13:52:09 +0530

    cart outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./components/NavBar";
+import CartContainer from "./components/CartContainer";
 function App() {
   return (
     <div>
       <NavBar />
+      <CartContainer />
     </div>
   );
 }
